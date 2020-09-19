909487d1fa65d3b3c9a82b8b35ee77bdda07504b
99002629 <71481318+99002629@users.noreply.github.com>
2020-09-19 13:51:39 +0530

    Create head.h

---
+++ head/head.h
@@ -0,0 +1,14 @@
+#ifndef __HEAD_H_H__
+#define __HEAD_H_H__
+
+
+void login();
+void mainMenu();
+void checkBalance(float balance);
+float moneyDeposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit();
+void errorMessage();
+
+#endif /* #ifndef __HEAD_H_H__ */
+
